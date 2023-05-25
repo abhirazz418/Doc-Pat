79aa44a60e7be5e1c811bc0d9a1eeb41f95d8a90
Abhay Kumar Modi <abhirazz418@gmail.com>
2023-05-25 13:38:21 +0530

    name changed

---
+++ upchaar-web-master/core/urls.py
@@ -0,0 +1,45 @@
+from django.urls import path, re_path, include
+from . import views
+from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'contacts', views.ContactViewSet)
+router.register(r'departments', views.DepartmentViewSet)
+router.register(r'doctors', views.DoctorViewSet)
+router.register(r'patients', views.PatientViewSet)
+router.register(r'hospitals', views.HospitalViewSet)
+router.register(r'dayschedule', views.DayScheduleViewSet)
+router.register(r'appointment', views.AppointmentViewSet)
+router.register(r'notifications', views.NotificationViewSet)
+
+urlpatterns = [
+    path('testUI/', views.testUI, name='testUI'),
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('signup/patient', views.signup_patient, name='signup_patient'),
+    path('signup/doctor', views.signup_doctor, name='signup_doctor'),
+    path('signup/hospital', views.signup_hospital, name='signup_hospital'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/patient', views.dashboard_patient, name='dashboard_patient'),
+    path('dashboard/doctor', views.dashboard_doctor, name='dashboard_doctor'),
+    path('dashboard/hospital', views.dashboard_hospital, name='dashboard_hospital'),
+    path('appointment/book', views.book_appointment, name='book_appointment'),
+    path('appointment/filter', views.filter_appointment, name='filter_appointment'),
+    path('appointment/calendar', views.calendar_appointment, name='calendar_appointment'),
+    path('appointment/upcoming', views.upcoming_appointment, name='upcoming_appointment'),
+    path('doctor/leave', views.doctor_leave, name='doctor_leave'),
+    path('doctor/leave/reschedule', views.doctor_leave_after, name='doctor_leave_after'),
+    path('notifications', views.notifications, name='notifications'),
+    path('faq', views.faq, name='faq'),
+    path('legal', views.legal, name='legal'),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/docs/', include_docs_urls(title='UPCHAAR API', public=False)),
+    path('api/login/', views.api_login, name='api_login'),
+    path('api/logout/', views.api_logout, name='api_logout'),
+    path('api/auth-token/', views.get_auth_token, name='api_auth_token'),
+    path('api/send-sms/', views.send_sms, name='api_send_sms'),
+    path('api/send-notification/', views.send_notification, name='api_send_notification'),
+]
---
+++ upchaar-web-master/core/views.py
@@ -0,0 +1,346 @@
+from django.conf import settings
+from django.contrib.auth import login, authenticate
+from django.core import serializers as django_serializers
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django import forms
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view, detail_route, list_route
+from rest_framework.response import Response
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK
+from rest_framework.authtoken.models import Token
+from django.shortcuts import render
+from .models import User, Contact, Department, Doctor, Patient, Hospital, DaySchedule, Appointment, Notification
+from .serializers import *
+from .forms import UserSignUpForm, DoctorSignupForm, HospitalSignupForm
+import nexmo
+
+client = nexmo.Client(key='e43bbd7d', secret='w7mD1e7IoJCNOu3U')
+
+
+@api_view(['POST'])
+def api_login(request, user=None):
+    if not user:
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+    if not user:
+        return Response({'error': 'Login Failed'}, status=HTTP_401_UNAUTHORIZED)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({
+        'id': user.id,
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+        'role': user.role,
+        'token': token.key})
+
+
+@api_view(['POST'])
+def api_logout(request):
+    id = request.data.get('id')
+    if not id:
+        return Response({'error': 'Invalid Request'}, status=HTTP_401_UNAUTHORIZED)
+
+    try:
+        user = User.objects.get(id=id)
+        auth_token = user.auth_token
+    except:
+        return Response({'error': 'Invalid Request'}, status=HTTP_401_UNAUTHORIZED)
+
+    user.auth_token.delete()
+    return Response({'id': id, 'token': auth_token.key}, status=HTTP_200_OK)
+
+
+@api_view(['POST'])
+def get_auth_token(request):
+    id = request.data.get('id')
+    if not id:
+        return Response({'error': 'Invalid Request'}, status=HTTP_401_UNAUTHORIZED)
+
+    try:
+        user = User.objects.get(id=id)
+        auth_token = user.auth_token
+    except:
+        return Response({'error': 'Invalid Request'}, status=HTTP_401_UNAUTHORIZED)
+
+    return Response({'id': id, 'token': auth_token.key}, status=HTTP_200_OK)
+
+
+@api_view(['POST'])
+def send_sms(request):
+    phone = request.data.get('phone')
+    message = request.data.get('message')
+    client.send_message({
+        'from': 'UPCHAAR',
+        'to': phone,
+        'text': message,
+    })
+    return Response({'message': 'Success'})
+
+
+@api_view(['POST'])
+def send_notification(request):
+    message = request.data.get('message')
+    user_id = request.data.get('user_id')
+    user = get_object_or_404(User, id=user_id)
+    notification = Notification(user=user, message=message)
+    notification.save()
+    return Response({'message': 'Success'})
+
+
+def index(request):
+    unread_notifications = Notification.objects.filter(read=False)
+    return render(request=request, template_name='index.html', context={'unread_notifications':unread_notifications})
+
+
+def signup(request):
+    return render(request=request, template_name='signup/signup.html')
+
+
+def signup_patient(request):
+    if request.method == 'POST':
+        userform = UserSignUpForm(request.POST, prefix='userform')
+        if userform.is_valid():
+            user = userform.save()
+            user.role = User.PATIENT
+            user.date_of_birth = request.POST.get('date_of_birth')
+            user.save()
+            patient = Patient(user=user)
+            patient.save()
+            username = userform.cleaned_data.get('username')
+            raw_password = userform.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        userform = UserSignUpForm(prefix='userform')
+        # patientform = PatientSignupForm(request.POST, prefix='patientform')
+    return render(request=request, template_name='signup/signup_patient.html',
+                  context={'userform': userform})
+
+
+def signup_doctor(request):
+    departments = Department.objects.all()
+    if request.method == 'POST':
+        userform = UserSignUpForm(request.POST, prefix='userform')
+        doctorform = DoctorSignupForm(request.POST, prefix='doctorform')
+        if userform.is_valid() and doctorform.is_valid():
+            user = userform.save()
+            user.role = User.DOCTOR
+            user.date_of_birth = request.POST.get('date_of_birth')
+            user.save()
+            doctor = doctorform.save()
+            doctor.user = user
+            doctor.department = request.POST.get('department')
+            doctor.time_slot = 20
+            doctor.save()
+            username = userform.cleaned_data.get('username')
+            raw_password = userform.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        userform = UserSignUpForm(prefix='userform')
+        doctorform = DoctorSignupForm(request.POST, prefix='doctorform')
+    return render(request=request, template_name='signup/signup_doctor.html',
+                  context={'userform': userform, 'doctorform': doctorform, 'departments':departments})
+
+
+def signup_hospital(request):
+    if request.method == 'POST':
+        userform = UserSignUpForm(request.POST, prefix='userform')
+        userform.fields['first_name'].widget = forms.HiddenInput()
+        userform.fields['last_name'].widget = forms.HiddenInput()
+        hospitalform = HospitalSignupForm(request.POST, prefix='hospitalform')
+        if userform.is_valid() and hospitalform.is_valid():
+            user = userform.save()
+            user.role = User.HOSPITAL
+            user.save()
+            hospital = hospitalform.save()
+            hospital.user = user
+            hospital.save()
+            username = userform.cleaned_data.get('username')
+            raw_password = userform.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        userform = UserSignUpForm(prefix='userform')
+        userform.fields['first_name'].widget = forms.HiddenInput()
+        userform.fields['last_name'].widget = forms.HiddenInput()
+        hospitalform = HospitalSignupForm(request.POST, prefix='hospitalform')
+    return render(request=request, template_name='signup/signup_hospital.html',
+                  context={'userform': userform, 'hospitalform': hospitalform})
+
+
+def dashboard(request):
+    if request.user.role == User.PATIENT:
+        return dashboard_patient(request)
+    elif request.user.role == User.DOCTOR:
+        return dashboard_doctor(request)
+    else:
+        return dashboard_hospital(request)
+
+
+def dashboard_patient(request):
+    return render(request=request, template_name='dashboard/dashboard_patient.html')
+
+
+def dashboard_doctor(request):
+    return render(request=request, template_name='dashboard/dashboard_doctor.html')
+
+
+def dashboard_hospital(request):
+    return render(request=request, template_name='dashboard/dashboard_hospital.html')
+
+
+def book_appointment(request):
+    return render(request=request, template_name='book_appointment_map.html')
+
+
+def filter_appointment(request):
+    users = django_serializers.serialize('json', User.objects.all())
+    contacts = django_serializers.serialize('json', Contact.objects.all())
+    departments = django_serializers.serialize('json', Department.objects.all())
+    doctors = django_serializers.serialize('json', Doctor.objects.all())
+    patients = django_serializers.serialize('json', Patient.objects.all())
+    dayschedules = django_serializers.serialize('json', DaySchedule.objects.all())
+    appointments = django_serializers.serialize('json', Appointment.objects.all())
+    hospitals = django_serializers.serialize('json', Hospital.objects.all())
+    return render(request=request, template_name='filter_appointment.html',
+                  context={'users': users,
+                           'contacts': contacts,
+                           'departments': departments,
+                           'doctors': doctors,
+                           'patients': patients,
+                           'hospitals': hospitals,
+                           'daySchedule': dayschedules,
+                           'appointments': appointments
+                           })
+
+
+def calendar_appointment(request):
+    dayschedules = django_serializers.serialize('json', DaySchedule.objects.all())
+    appointments = django_serializers.serialize('json', Appointment.objects.all())
+    return render(request=request, template_name='calendar.html',
+                  context={'dayschedules': dayschedules, 'appointments':appointments})
+
+
+def upcoming_appointment(request):
+    appointments = django_serializers.serialize('json', Appointment.objects.all())
+    return render(request=request, template_name='upcoming_appointment.html',
+                  context={'appointments': appointments})
+
+
+def doctor_leave(request):
+    if request.method == 'POST':
+        print('in')
+        date = request.POST.get('leavedate')
+        doctor = Doctor(user=request.user)
+        appointments = Appointment.objects.filter(appointment_date=date)
+        for appointment in appointments:
+            user = appointment.patient
+            message = 'Your appointment has to be rescheduled due to cancellation by Dr. {}. '.format(doctor)
+            message += 'Alternative recommended option: {}, {} at {}'.format(
+                'Dr. Abhay Mahajan', appointment.Hospital, appointment.date)
+            message += 'To approve/modify your appointment, visit your dashboard'
+            print(message)
+            Notification.objects.create(user_id=user.id, message=message)
+            phone = 919211277070
+            message = request.data.get('message')
+            client.send_message({
+                'from': 'UPCHAAR',
+                'to': phone,
+                'text': message,
+            })
+        print('done')
+        return redirect(reverse('doctor_leave_after'))
+    return render(request=request, template_name='doctor_leave.html')
+
+
+def notifications(request):
+    unread_notifications = Notification.objects.filter(read=False)
+    read_notifications = Notification.objects.filter(read=True)
+    for notification in unread_notifications:
+        notification.read = True
+        notification.save()
+    return render(request=request, template_name='notifications.html',
+                  context={'unread_notifications':unread_notifications, 'read_notifications':read_notifications})
+
+
+def doctor_leave_after(request):
+    return render(request=request, template_name='doctor_leave_after.html')
+
+
+def faq(request):
+    return render(request=request, template_name='FAQ.html')
+
+
+def legal(request):
+    return render(request=request, template_name='legal.html')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class HospitalViewSet(viewsets.ModelViewSet):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+
+
+class DayScheduleViewSet(viewsets.ModelViewSet):
+    queryset = DaySchedule.objects.all()
+    serializer_class = DayScheduleSerializer
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+
+def testUI(request):
+    users = django_serializers.serialize('json', User.objects.all())
+    contacts = django_serializers.serialize('json', Contact.objects.all())
+    departments = django_serializers.serialize('json', Department.objects.all())
+    doctors = django_serializers.serialize('json', Doctor.objects.all())
+    patients = django_serializers.serialize('json', Patient.objects.all())
+    appointments = django_serializers.serialize('json', Appointment.objects.all())
+    hospitals = django_serializers.serialize('json', Hospital.objects.all())
+    return render(request=request, template_name='testUI.html',
+                  context={'users': users,
+                           'contacts': contacts,
+                           'departments': departments,
+                           'doctors': doctors,
+                           'patients': patients,
+                           'hospitals': hospitals,
+                           'appointments':appointments
+                           })
